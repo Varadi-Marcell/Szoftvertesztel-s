e1d285c42a4ba0b1ab37b97b2e37066e7e1015a7
Varadi-Marcell <marcibazsi11@gmail.com>
2021-10-08 16:32:00 +0200

    orai junit teszt

---
+++ Bundle/core/src/test/java/hu/uni/miskolc/teszteles/core/AppTest.java
@@ -0,0 +1,16 @@
+package hu.uni.miskolc.teszteles.core;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+public class AppTest 
+{
+  
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
